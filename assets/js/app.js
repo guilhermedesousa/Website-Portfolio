f743c5633b04f4f0c1c9ba0e278617a0bae4a56c
guilhermedesousa <sousa.santos@aluno.ufabc.edu.br>
2021-05-16 00:39:27 -0300

    Introduz alguns efeitos e estilizações à página

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,34 @@ $('.smooth-scroll').on('click', function (e) {
         scrollTop: targetOffset - menuHeight
     }, 1000)
 })
+
+// Scroll top
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(window).scrollTop() > 1200) {
+            $('.scrollUp').css({
+                "opacity":"1", "pointer-events":"auto"
+            })
+        } else {
+            $('.scrollUp').css({
+                "opacity":"0", "pointer-events":"none"
+            })
+        }
+    })
+    $('.scrollUp').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000)
+    })
+})
+
+// Type writer
+function typeWriter(element) {
+    const textArray = element.innerHTML.split('')
+    element.innerHTML = ''
+    textArray.forEach((letter, index) => {
+        setTimeout(() => element.innerHTML += letter, 90 * index)
+    });
+}
+const title = document.querySelector('.introduction h3')
+typeWriter(title)
--- assets/js/parallax.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    parallax()
-})
-
-function parallax() {
-    const wScroll = $(window).scrollTop()
-    
-    $('.parallax--bg').css('background-position', 'center top '+(wScroll*0.5)+'px')
-}
